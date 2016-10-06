007e34159ebcf539328a121ee98028de185fd047
Mez Pahlan <mez.pahlan@gmail.com>
2016-10-06 20:00:55 +0100

    Always set the API key on requests.

--- app/src/main/java/uk/co/mezpahlan/jamesedwardtaylorstheguardian/data/GuardianOpenPlatformServiceGenerator.java
+++ app/src/main/java/uk/co/mezpahlan/jamesedwardtaylorstheguardian/data/GuardianOpenPlatformServiceGenerator.java
@@ -1,8 +1,15 @@
 package uk.co.mezpahlan.jamesedwardtaylorstheguardian.data;
 
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import uk.co.mezpahlan.jamesedwardtaylorstheguardian.base.Constants;
 
 /**
  * Retrofit Service Generator for The Guardian Open Platform.
@@ -17,6 +24,27 @@ public class GuardianOpenPlatformServiceGenerator {
             .addConverterFactory(GsonConverterFactory.create());
 
     public static <S> S createService(Class<S> serviceClass) {
+
+        // Always add the API key to requests
+        httpClient.addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request original = chain.request();
+                HttpUrl originalHttpUrl = original.url();
+
+                HttpUrl url = originalHttpUrl.newBuilder()
+                        .addQueryParameter("api-key", Constants.GUARDIAN_KEY)
+                        .build();
+
+                // Request customization: add request headers
+                Request.Builder requestBuilder = original.newBuilder()
+                        .url(url);
+
+                Request request = requestBuilder.build();
+                return chain.proceed(request);
+            }
+        });
+
         Retrofit retrofit = builder.client(httpClient.build()).build();
         return retrofit.create(serviceClass);
     }
