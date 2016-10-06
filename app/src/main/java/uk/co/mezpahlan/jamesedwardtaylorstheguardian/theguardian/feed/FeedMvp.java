8eca7b3370bd316f847de2b569f97f67fdfa70ef
Mez Pahlan <mez.pahlan@gmail.com>
2016-10-06 17:10:01 +0100

    MVP interface triad for the Feed component.

---
+++ app/src/main/java/uk/co/mezpahlan/jamesedwardtaylorstheguardian/theguardian/feed/FeedMvp.java
@@ -0,0 +1,27 @@
+package uk.co.mezpahlan.jamesedwardtaylorstheguardian.theguardian.feed;
+
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+import uk.co.mezpahlan.jamesedwardtaylorstheguardian.base.BaseMvp;
+import uk.co.mezpahlan.jamesedwardtaylorstheguardian.data.model.Response;
+import uk.co.mezpahlan.jamesedwardtaylorstheguardian.data.model.Result;
+
+/**
+ * MVP definitions for RSS Feed.
+ */
+public interface FeedMvp {
+    interface View extends BaseMvp.LCEView <List<Result>> {
+        void showGuardianArticle();
+    }
+
+    interface Presenter extends BaseMvp.Presenter <List<Result>> {
+        void onConfigurationChanged(View view);
+        void onSelectRssItem(@NonNull Result result);
+    }
+
+    interface ModelInteractor extends BaseMvp.ModelInteractor <Response> {
+        void fetchCached();
+    }
+}
