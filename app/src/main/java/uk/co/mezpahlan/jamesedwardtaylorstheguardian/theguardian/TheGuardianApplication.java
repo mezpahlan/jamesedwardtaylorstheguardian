3515d5d99bd14f1a77bfa524a6991e115372b6cd
Mez Pahlan <mez.pahlan@gmail.com>
2016-10-08 18:17:18 +0100

    Style the Toolbar

--- app/src/main/java/uk/co/mezpahlan/jamesedwardtaylorstheguardian/theguardian/TheGuardianApplication.java
+++ app/src/main/java/uk/co/mezpahlan/jamesedwardtaylorstheguardian/theguardian/TheGuardianApplication.java
@@ -3,7 +3,6 @@ package uk.co.mezpahlan.jamesedwardtaylorstheguardian.theguardian;
 import android.app.Application;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
-import uk.co.mezpahlan.jamesedwardtaylorstheguardian.R;
 
 /**
  * Application for TheGuardian. Sets up a custom typeface throughout the app.
@@ -15,7 +14,6 @@ public class TheGuardianApplication extends Application {
         super.onCreate();
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                 .setDefaultFontPath("fonts/Merriweather-Regular.ttf")
-                .setFontAttrId(R.attr.fontPath)
                 .build()
         );
     }
