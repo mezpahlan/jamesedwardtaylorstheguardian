b91dd93337ba662fcee35eb7e6038413cb43dce8
Mez Pahlan <mez.pahlan@gmail.com>
2016-10-08 17:01:40 +0100

    Allow custom fonts in the Article component

--- app/src/main/java/uk/co/mezpahlan/jamesedwardtaylorstheguardian/theguardian/article/ArticleActivity.java
+++ app/src/main/java/uk/co/mezpahlan/jamesedwardtaylorstheguardian/theguardian/article/ArticleActivity.java
@@ -3,11 +3,13 @@ package uk.co.mezpahlan.jamesedwardtaylorstheguardian.theguardian.article;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 import uk.co.mezpahlan.jamesedwardtaylorstheguardian.R;
 
 /**
@@ -43,5 +45,10 @@ public class ArticleActivity extends AppCompatActivity {
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.add(R.id.feed_frame_view, feedFragment);
         transaction.commit();
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
