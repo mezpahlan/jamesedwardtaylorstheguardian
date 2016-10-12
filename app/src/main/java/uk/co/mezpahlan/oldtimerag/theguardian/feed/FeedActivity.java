51f5c077704cf82a582ee0765f1b7ace00f9e090
Mez Pahlan <mez.pahlan@gmail.com>
2016-10-12 18:02:53 +0100

    Loading screen

--- app/src/main/java/uk/co/mezpahlan/oldtimerag/theguardian/feed/FeedActivity.java
+++ app/src/main/java/uk/co/mezpahlan/oldtimerag/theguardian/feed/FeedActivity.java
@@ -24,6 +24,7 @@ public class FeedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theguardian_feed);
 
