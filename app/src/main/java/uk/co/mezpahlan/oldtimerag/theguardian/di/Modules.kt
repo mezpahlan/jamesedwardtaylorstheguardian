1b8764422704ac568dcb95708332ffb4bfeeba64
Mez Pahlan <mez.pahlan@gmail.com>
2018-08-27 21:58:15 +0100

    Add DI module

---
+++ app/src/main/java/uk/co/mezpahlan/oldtimerag/theguardian/di/Modules.kt
@@ -0,0 +1,22 @@
+package uk.co.mezpahlan.oldtimerag.theguardian.di
+
+import org.koin.android.viewmodel.ext.koin.viewModel
+import org.koin.dsl.module.module
+import uk.co.mezpahlan.oldtimerag.theguardian.data.TheGuardianRepository
+import uk.co.mezpahlan.oldtimerag.theguardian.data.network.TheGuardianOpenPlatformServiceGenerator
+import uk.co.mezpahlan.oldtimerag.theguardian.viewmodels.SharedViewModel
+
+val theGuardianModule = module {
+    single { TheGuardianRepository(get("feed_service"), get("article_service")) }
+
+    viewModel { SharedViewModel(get()) }
+}
+
+val networkModule = module {
+
+    factory("feed_service") { TheGuardianOpenPlatformServiceGenerator.createFeedService() }
+
+    factory("article_service") { TheGuardianOpenPlatformServiceGenerator.createArticleService() }
+}
+
+val appModules = listOf(theGuardianModule, networkModule)
