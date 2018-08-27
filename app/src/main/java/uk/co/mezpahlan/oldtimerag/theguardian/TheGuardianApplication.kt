722fe879fc0913b178a6cd660922a2a949835da6
Mez Pahlan <mez.pahlan@gmail.com>
2018-08-27 22:00:02 +0100

    Start Koin

--- app/src/main/java/uk/co/mezpahlan/oldtimerag/theguardian/TheGuardianApplication.kt
+++ app/src/main/java/uk/co/mezpahlan/oldtimerag/theguardian/TheGuardianApplication.kt
@@ -2,7 +2,9 @@ package uk.co.mezpahlan.oldtimerag.theguardian
 
 
 import android.app.Application
+import org.koin.android.ext.android.startKoin
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig
+import uk.co.mezpahlan.oldtimerag.theguardian.di.appModules
 
 /**
  * Application for TheGuardian. Sets up a custom typeface throughout the app.
@@ -11,6 +13,9 @@ class TheGuardianApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        startKoin(this, appModules)
+
         CalligraphyConfig.initDefault(CalligraphyConfig.Builder()
                 .setDefaultFontPath("fonts/Merriweather-Regular.ttf")
                 .build()
