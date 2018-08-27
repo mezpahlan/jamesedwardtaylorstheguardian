d9dd1962424c7e19c8161b7e1009f4cc8e520227
Mez Pahlan <mez.pahlan@gmail.com>
2018-08-27 18:53:41 +0100

    Rename and move GuardianOpenPlatform API classes

--- app/src/main/java/uk/co/mezpahlan/oldtimerag/theguardian/data/network/TheGuardianOpenPlatformClient.kt
+++ app/src/main/java/uk/co/mezpahlan/oldtimerag/theguardian/data/network/TheGuardianOpenPlatformClient.kt
@@ -1,16 +1,16 @@
-package uk.co.mezpahlan.oldtimerag.data
+package uk.co.mezpahlan.oldtimerag.theguardian.data.network
 
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 import retrofit2.http.Url
-import uk.co.mezpahlan.oldtimerag.data.model.search.Search
-import uk.co.mezpahlan.oldtimerag.data.model.singleitem.SingleItem
+import uk.co.mezpahlan.oldtimerag.theguardian.data.models.search.Search
+import uk.co.mezpahlan.oldtimerag.theguardian.data.models.singleitem.SingleItem
 
 /**
  * Retrofit client for The Guardian Open Platform Client
  */
-interface GuardianOpenPlatformClient {
+interface TheGuardianOpenPlatformClient {
     @GET("search")
     fun search(@Query("type") type: String): Single<Search>
 
